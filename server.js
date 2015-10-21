fe0489febba4e2ef8416165a777823712f981b79
Ehsan Tabatabaei <ehsantaba2010@gmail.com>
2015-10-21 14:56:44 -0700

    Added bar-chart. Browser renders files, chart does not get loaded

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/precipitation', function (req, res) {
+  res.header('Access-Control-Allow-Origin', '*');
   res.sendFile(__dirname + '/app/precipitation/json/precipitation.data.json');
   console.log('testing sample data')
 });
@@ -14,6 +15,8 @@ app.get('/reservoirs', function (req, res) {
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/public/dist'));
 app.use('/app/chart-app/', express.static(__dirname + '/app/chart-app/'));
+app.use('/app/precipitation/', express.static(__dirname + '/app/precipitation/'));
+app.use('/app/reservoirs/', express.static(__dirname + '/app/reservoirs/'));
 
 app.listen(1337);
 console.log('Connected!');
